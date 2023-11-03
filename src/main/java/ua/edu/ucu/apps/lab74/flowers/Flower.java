b848975b6bf17ef88f2e20e905add5b4840903da
ayrinnnx <kokhan.pn@ucu.edu.ua>
2023-11-03 22:53:19 +0200

    Fixed noargsconstructor
    Signed-off-by: ayrinnnx <kokhan.pn@ucu.edu.ua>

--- src/main/java/ua/edu/ucu/apps/lab74/flowers/Flower.java
+++ src/main/java/ua/edu/ucu/apps/lab74/flowers/Flower.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor @Getter
 @Entity
+@NoArgsConstructor
 public class Flower {
     @Id
     private int id;
